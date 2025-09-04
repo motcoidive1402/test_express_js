d22a64568884e02e938ebc142fcbb3985b9ba50b
Son Pham <motcoidive1402@yahoo.com>
2025-09-04 15:15:54 +0700

    update index api v1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Chào mừng bạn đến với website Express.js!');
+  res.send('Chào mừng bạn đến với website Express.js! abc');
 });
 
 app.get('/about', (req, res) => {
@@ -13,3 +13,13 @@ app.get('/about', (req, res) => {
 app.listen(port, () => {
   console.log(`Ứng dụng đang chạy trên cổng ${port}`);
 });
+
+const router1 = express.Router();
+router1.get('/product',(req,res)=>{
+	res.json('router 1 product')
+
+})
+router1.get('/cart',(req,res)=>{
+	res.json(' cart retouer 1')
+})
+app.use('/api/v1/', router1);
