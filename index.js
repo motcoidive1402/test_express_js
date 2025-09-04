48408fc8739eb14788535dae76557681f914fc0f
Son Pham <motcoidive1402@yahoo.com>
2025-09-04 13:14:07 +0700

    update index js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Chào mừng bạn đến với website Express.js!');
+});
+
+app.get('/about', (req, res) => {
+  res.send('Đây là trang giới thiệu.');
+});
+
+app.listen(port, () => {
+  console.log(`Ứng dụng đang chạy trên cổng ${port}`);
+});
